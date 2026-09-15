dde58556710a2d238b06b33804351fbf932891fb
Noturno <rodrigodqbrn@gmail.com>
2026-09-15 20:43:49 -0300

    atualiza  orepositório

---
+++ aulas/POO/EstruturasDeClasses.py
@@ -0,0 +1,6 @@
+# POO -> Programação Orientada a Objetos
+
+#Toda classe precisa da palavra reservada class
+
+class Aluno: #toda classe começa com a letra maiúscula
+    pass # significado: eu vou escrever alguma coisa aqui futuramente
